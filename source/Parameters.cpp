9886acb45fcd50b3d2c539a9acba43634b814600
MRSDN <business@kingbnjmn.com>
2025-09-28 14:29:40 -0500

    added ms/s logic for delay time user input

--- source/Parameters.cpp
+++ source/Parameters.cpp
@@ -41,6 +41,20 @@ static juce::String stringFromMilliseconds(float value, int)
     }
 }
 
+static float millisecondsFromString( const juce::String& text)
+{
+    float value = text.getFloatValue();
+
+    if (!text.endsWithIgnoreCase("ms"))
+    {
+        if (text.endsWithIgnoreCase("s") || value < Parameters::minDelayTime)
+        {
+            return value * 1000.0f;
+        }
+    }
+    return value;
+}
+
 static juce::String stringFromDecibels(float value, int)
 {
     return juce::String(value, 1) + "dB";
@@ -63,7 +77,10 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
 
     layout.add(std::make_unique<juce::AudioParameterFloat>(delayTimeParamID, "Delay Time",
         juce::NormalisableRange<float> { minDelayTime, maxDelayTime, 0.001f, 0.25f}, 100.0f,
-        juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromMilliseconds)));
+        juce::AudioParameterFloatAttributes()
+        .withStringFromValueFunction(stringFromMilliseconds)
+        .withValueFromStringFunction(millisecondsFromString)));
+
 
     layout.add(std::make_unique<juce::AudioParameterFloat>(mixParamID, "Mix",
         juce::NormalisableRange<float> {0.0f, 100.0f, 1.0f}, 100.0f,
